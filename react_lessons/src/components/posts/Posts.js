3267728975638d23dd0ec63b08db84a717f69b8c
help4engineer <help4engineer@gmail.com>
2020-12-07 20:22:13 +0200

    React lesson3 practice

--- react_lessons/src/components/posts/Posts.js
+++ react_lessons/src/components/posts/Posts.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
-import Users from "./components/users/Users";
 
-class App extends Component {
+class Posts extends Component {
     render() {
         return (
             <div>
-              <Users />
-
+                
             </div>
         );
     }
 }
 
-export default App;
+export default Posts;
--- react_lessons/src/components/posts/post/Post.js
+++ react_lessons/src/components/posts/post/Post.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
-import Users from "./components/users/Users";
 
-class App extends Component {
+class Post extends Component {
     render() {
         return (
             <div>
-              <Users />
-
+                
             </div>
         );
     }
 }
 
-export default App;
+export default Post;
