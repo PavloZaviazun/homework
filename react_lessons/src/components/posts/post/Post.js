c28d6c1d54ba57130cef6df8996b72faaa8000eb
help4engineer <help4engineer@gmail.com>
2020-12-11 15:56:55 +0200

    React_lesson3_practice

--- react_lessons/src/components/posts/post/Post.js
+++ react_lessons/src/components/posts/post/Post.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 
 class Post extends Component {
     render() {
+        let {post} = this.props;
+
         return (
             <div>
-                
+                {post.title}<br/>
+                {post.body}
             </div>
         );
     }
--- react_lessons/src/components/users/Users.js
+++ react_lessons/src/components/users/Users.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import {UserService} from "../../services/UserService";
 import User from "./user/User";
-import Posts from "../posts/Posts";
+import {PostService} from "../../services/PostService";
+import Post from "../posts/post/Post";
 
 class Users extends Component {
 
     userService = new UserService();
-    posts = new Posts();
+    postService = new PostService();
 
-    state = {users: []};
+    state = {users: [], post: null};
     render() {
-        let {users} = this.state;
+        let {users, post} = this.state;
         return (
             <div>
                 {
@@ -22,12 +23,25 @@ class Users extends Component {
                         />
                     })
                 }
+                {
+                    post && <Post key={post.id} post={post}/>
+                }
             </div>
         );
     }
 
     getID = (id) => {
-        this.posts.getPostsOfUsers(id);
+        let random = Math.random()*((10 - 1) + 1);
+        console.log(Math.ceil(random))
+        let defPost;
+        this.postService.getPosts(id).then(posts => posts.find( post => {
+            if(post.id % 10) {
+                if (Math.ceil(random) + (Math.floor(post.id / 10) * 10) === post.id) defPost = post;
+            } else {
+                if (Math.ceil(random) + (post.id / 10 - 1) * 10 === post.id) defPost = post;
+            }
+            return defPost;
+        })).then(value => this.setState({post: value}))
     }
 
     componentDidMount() {
