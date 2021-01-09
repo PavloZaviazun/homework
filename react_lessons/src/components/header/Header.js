7558cc6b75943721715199576a14f3770c87a9b0
help4engineer <help4engineer@gmail.com>
2021-01-09 15:25:19 +0200

    React_lesson_rozetka

--- react_lessons/src/components/header/Header.js
+++ react_lessons/src/components/header/Header.js
@@ -1,10 +1,17 @@
 import "./Header.css";
+import topBanner from  "../../images/top-banner.jpg";
+
 
 export const Header = () => {
 
     return(
         <header>
+            <div className={"top-banner"}>
+                <img src={topBanner} alt={"Everything for cozy holidays"}/>
+            </div>
+        <div className={"header-body"}>
             Header
+        </div>
         </header>
     )
 }
