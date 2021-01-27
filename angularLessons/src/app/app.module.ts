bc362cb0f2d051d848da2d40cae9f3de2c573a13
help4engineer <help4engineer@gmail.com>
2021-01-27 17:57:23 +0200

    angular_lesson4

--- angularLessons/src/app/app.module.ts
+++ angularLessons/src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { FullPostComponent } from './components/full-post/full-post.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import {PostResolveService} from './services/post-resolve.service';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'posts', component: PostsComponent, resolve: {postsData: PostResolveService}, children: [
+      {path: ':id', component: FullPostComponent}
+    ]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    FullPostComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
