7416dd286f45c5ac03fc39451ff6d9ddbe4cc5e6
help4engineer <help4engineer@gmail.com>
2021-02-03 20:59:16 +0200

    angular_lesson7_practice

--- angularLessons/src/app/app.module.ts
+++ angularLessons/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { CarsComponent } from './components/main/cars/cars.component';
+import { CreationComponent } from './components/main/creation/creation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CarsComponent,
+    CreationComponent
   ],
   imports: [
     BrowserModule
