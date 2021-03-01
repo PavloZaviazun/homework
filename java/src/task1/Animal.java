e44d46ad27213160ca9f52fd979d76449937f446
help4engineer <help4engineer@gmail.com>
2021-03-01 12:28:54 +0200

    Java_lesson2

---
+++ java/src/task1/Animal.java
@@ -0,0 +1,11 @@
+package task1;
+
+public abstract class Animal {
+    private String name;
+    private int maxAge;
+
+    public Animal(String name, int maxAge) {
+        this.name = name;
+        this.maxAge = maxAge;
+    }
+}
