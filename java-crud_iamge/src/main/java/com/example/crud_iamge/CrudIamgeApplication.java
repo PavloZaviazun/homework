0e27348e1d49fbd981de7ec580c064218e136b0c
Pavlo Zaviazun <help4engineer@gmail.com>
2021-03-23 13:25:20 +0200

    java_lesson10_practice

---
+++ java-crud_iamge/src/main/java/com/example/crud_iamge/CrudIamgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_iamge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudIamgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudIamgeApplication.class, args);
+    }
+
+}
---
+++ java-crud_iamge/src/test/java/com/example/crud_iamge/CrudIamgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_iamge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudIamgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
