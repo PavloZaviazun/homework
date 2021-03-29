7554ae88f1fb5c94285ab678d8ae863e23d781e9
Pavlo Zaviazun <help4engineer@gmail.com>
2021-03-29 11:39:30 +0300

    TodoList

--- todoAngular/src/app/components/auth/auth.component.ts
+++ todoAngular/src/app/components/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private authService: AuthService) { }
+  username = new FormControl('');
+  password = new FormControl('');
+  formLogin = new FormGroup({
+    username: this.username,
+    password: this.password
+  });
   ngOnInit(): void {
   }
 
+  sendData(): void {
+    const formData = new FormData();
+    const name = this.formLogin.get('username').value;
+    const pass = this.formLogin.get('password').value;
+    const user = {username: name, password: pass};
+    this.authService.loginAttempt(user);
+  }
 }
--- todoAngular/src/app/config/urls.ts
+++ todoAngular/src/app/config/urls.ts
@@ -2,5 +2,6 @@ import {environment} from '../../environments/environment';
 
 export const URL = {
   lists: `${environment.SERVER}/todolists`,
-  list: `${environment.SERVER}/todolist`
+  list: `${environment.SERVER}/todolist`,
+  auth: `${environment.SERVER}/auth`
 };
