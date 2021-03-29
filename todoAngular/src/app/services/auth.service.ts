421c663e5681a5991b9926cc57e3b412e06bad40
Pavlo Zaviazun <help4engineer@gmail.com>
2021-03-29 11:39:49 +0300

    TodoList

---
+++ todoAngular/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {URL} from '../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  loginAttempt(user): void {
+    this.httpClient.post<string>(URL.auth, JSON.stringify(user) , {
+      headers: {Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiJ9.3tm1LU8zBo3-IxzmiwqsSH2ZI6Gh8Jifa0F8QCWqPxppHzjMwFgwuBgy0sdXF2ogzAx6RsrCeboheR5UnWZGHA'} }).subscribe(el => el);
+  }
+}
